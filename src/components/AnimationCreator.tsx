1618741871946d511e6bda7b8604703745fcf2c4
tomcorey26 <tomcorey26@gmail.com>
2020-03-05 15:23:58 -0500

    bruhhhhhh

--- src/components/AnimationCreator.tsx
+++ src/components/AnimationCreator.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef, useCallback } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import produce from "immer";
 
 import Point from "../interfaces/Point";
